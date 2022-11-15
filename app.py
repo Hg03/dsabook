dac83724454d98a914d0e81c245219af0ed5a05b
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2022-11-15 19:20:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 from streamlit_option_menu import option_menu
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title='DSABook',layout="wide")
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
